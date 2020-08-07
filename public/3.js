ff41e700186d73f27ec228226f499983bab87eb7
polaris <polaris.tiger803@yandex.com>
2020-08-07 21:03:26 +0300

    .gitignore fix

--- public/3.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{52:function(e,n,t){"use strict";t.r(n);var a={name:"layout"},p=t(9),r=t(10),u=t.n(r),c=t(143),o=Object(p.a)(a,(function(){var e=this.$createElement,n=this._self._c||e;return n("v-app",[n("router-view")],1)}),[],!1,null,"52c5e0d7",null);n.default=o.exports;u()(o,{VApp:c.a})}}]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/0.vuetify-components.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/js/app.vuetify-components.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 
     <script>
