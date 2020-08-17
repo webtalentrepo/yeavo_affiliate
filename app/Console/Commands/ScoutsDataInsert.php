a84b7556c8f476c8913f201eca39c8db154f87e3
polaris <polaris.tiger803@yandex.com>
2020-08-17 22:35:19 +0300

    first

--- app/Console/Commands/ScoutsDataInsert.php
+++ app/Console/Commands/ScoutsDataInsert.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Http\Repositories\CommissionJunction;
+use App\Http\Repositories\ScoutRepository;
 use App\Models\Product;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
@@ -40,17 +41,57 @@ class ScoutsDataInsert extends Command
      */
     public function handle()
     {
-        $links = ['clickbank.com', 'cj.com', 'jvzoo.com'];
+        $links = ['clickbank.com', 'cj.com'];
 
         for ($i = 0; $i < count($links); $i++) {
             if ($links[$i] == 'cj.com') {
-                $cj = new CommissionJunction();
-                $re = $cj->getMerchants([]);
+//                $cj = new CommissionJunction();
+//                $re = $cj->getMerchants([]);
+//
+////                Log::info(json_encode($re));
+//
+//                if ($re) {
+//                    $cj->saveDataToTable($re, $links[$i]);
+//                }
+            } elseif ($links[$i] == 'clickbank.com') {
+                $path = public_path('downloads');
+                chmod($path, 0777);
+                if (file_exists(public_path('downloads/marketplace_feed_v2.xml.zip'))) {
+                    unlink(public_path('downloads/marketplace_feed_v2.xml.zip'));
+                }
 
-//                Log::info(json_encode($re));
+                if (file_exists(public_path('downloads/marketplace_feed_v2.xml'))) {
+                    unlink(public_path('downloads/marketplace_feed_v2.xml'));
+                }
 
-                if ($re) {
-                    $cj->saveDataToTable($re, $links[$i]);
+                if (file_exists(public_path('downloads/marketplace_feed_v2.dtd'))) {
+                    unlink(public_path('downloads/marketplace_feed_v2.dtd'));
+                }
+
+                //https://accounts.clickbank.com/feeds/marketplace_feed_v2.xml.zip
+                $content = file_get_contents('https://accounts.clickbank.com/feeds/marketplace_feed_v2.xml.zip');
+
+                if (file_put_contents(public_path('downloads/marketplace_feed_v2.xml.zip'), $content)) {
+                    $zip = new \ZipArchive();
+                    $res = $zip->open(public_path('downloads/marketplace_feed_v2.xml.zip'));
+                    if ($res === TRUE) {
+                        $zip->extractTo($path);
+                        $zip->close();
+
+                        if (file_exists(public_path('downloads/marketplace_feed_v2.xml'))) {
+                            $re = file_get_contents(public_path('downloads/marketplace_feed_v2.xml'));
+
+                            $xml = \simplexml_load_string($re, null, LIBXML_NOERROR | LIBXML_NOWARNING | LIBXML_NOCDATA);
+
+                            $json = json_encode($xml);
+                            $array = json_decode($json, true);
+
+                            if ($array && isset($array['Category'])) {
+                                $cb = new ScoutRepository();
+                                $cb->setClickBankData($array['Category'], $links[$i]);
+                            }
+                        }
+                    }
                 }
             }
         }
--- app/Http/Repositories/ScoutRepository.php
+++ app/Http/Repositories/ScoutRepository.php
@@ -18,6 +18,70 @@ class ScoutRepository
         return app(Product::class);
     }
 
+    public function setClickBankData($re, $link)
+    {
+        foreach ($re as $row) {
+            $category = isset($row['Name']) ? $row['Name'] : '';
+
+            $child_category = '';
+            if (isset($row['Category']) && sizeof($row['Category']) > 0) {
+                foreach ($row['Category'] as $cc_row) {
+                    $child_category = isset($cc_row['Name']) ? $cc_row['Name'] : '';
+
+                    if (isset($cc_row['Site']) && sizeof($cc_row['Site']) > 0) {
+                        foreach ($cc_row['Site'] as $s_row) {
+                            $this->saveClickBankData($s_row, $link, $category, $child_category);
+                        }
+                    }
+                }
+            } else {
+                if (isset($row['Site']) && sizeof($row['Site']) > 0) {
+                    foreach ($row['Site'] as $s_row) {
+                        $this->saveClickBankData($s_row, $link, $category, $child_category);
+                    }
+                }
+            }
+        }
+    }
+
+    private function saveClickBankData($row, $link, $category, $child_category)
+    {
+        if (!isset($row['Id'])) {
+            return;
+        }
+
+        $scout = $this->model()->where('site_id', $row['Id'])
+            ->where('network', $link)
+            ->first();
+
+        if ($scout) {
+            if ($scout->deleted_flag || $scout->edited_flag) {
+                return;
+            }
+        } else {
+            $scout = new Product();
+        }
+
+        $scout->network = $link;
+        $scout->category = is_array($category) ? json_encode($category) : $category;
+        $scout->child_category = is_array($child_category) ? json_encode($child_category) : $child_category;
+        $scout->site_id = is_array($row['Id']) ? json_encode($row['Id']) : $row['Id'];
+        $scout->popular_rank = isset($row['PopularityRank']) ? (is_array($row['PopularityRank']) ? json_encode($row['PopularityRank']) : $row['PopularityRank']) : 0;
+        $scout->p_title = isset($row['Title']) ? (is_array($row['Title']) ? json_encode($row['Title']) : $row['Title']) : '';
+        $scout->p_description = isset($row['Description']) ? (is_array($row['Description']) ? json_encode($row['Description']) : $row['Description']) : '';
+        $scout->p_commission = isset($row['Commission']) ? (is_array($row['Commission']) ? json_encode($row['Commission']) : $row['Commission']) : 0;
+        $scout->p_commission_unit = '%';
+        $scout->p_gravity = isset($row['Gravity']) ? (is_array($row['Gravity']) ? json_encode($row['Gravity']) : $row['Gravity']) : 0;
+        $scout->seven_day_epc = isset($row['AverageEarningsPerSale']) ? (is_array($row['AverageEarningsPerSale']) ? json_encode($row['AverageEarningsPerSale']) : $row['AverageEarningsPerSale']) : 0;
+        $scout->three_month_epc = isset($row['InitialEarningsPerSale']) ? (is_array($row['InitialEarningsPerSale']) ? json_encode($row['InitialEarningsPerSale']) : $row['InitialEarningsPerSale']) : 0;
+        $scout->earning_uint = 'USD';
+        $scout->p_percent_sale = isset($row['PercentPerSale']) ? (is_array($row['PercentPerSale']) ? json_encode($row['PercentPerSale']) : $row['PercentPerSale']) : 0;
+        $scout->deleted_flag = 0;
+        $scout->edited_flag = 0;
+
+        $scout->save();
+    }
+
     /**
      * @param $params
      * @param $sel_network
