b9fe8b98f66db006f4a3b6915f3c2eda434d7e8f
polaris <polaris.tiger803@yandex.com>
2020-08-18 19:51:42 +0300

    first

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('scoutsdata:insert')->dailyAt('06:00');
+         $schedule->command('scoutsdata:insert')
+             ->everyFourHours()
+             ->runInBackground();
     }
 
     /**
