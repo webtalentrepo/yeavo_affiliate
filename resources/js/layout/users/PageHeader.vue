8e808ffaaadd127f02c35b041d94b66fde50c677
polaris <polaris.tiger803@yandex.com>
2020-09-06 01:02:14 +0300

    eslint config set up

--- resources/js/layout/users/PageHeader.vue
+++ resources/js/layout/users/PageHeader.vue
@@ -2,10 +2,10 @@
     <v-row
         justify="center"
         align="center"
-        class="mr-0 ml-0 text-h3 font-weight-thin page-header-title"
+        class="mr-0 ml-0 text-lg-h3 text-md-h4 text-sm-h5 font-weight-thin page-header-title"
     >
         <v-icon size="3rem" color="#282828">{{ icon }}</v-icon>
-        <div class="pt-8 pb-8 ml-3">
+        <div class="pt-5 pb-5 ml-3">
             {{ $route.name }}
         </div>
     </v-row>
@@ -39,5 +39,17 @@ export default {
 <style scoped lang="scss">
 .page-header-title {
     color: #282828;
+
+    @media (max-width: 480px) {
+        .v-icon {
+            font-size: 1.2rem !important;
+        }
+    }
+
+    @media (max-width: 1024px) {
+        .v-icon {
+            font-size: 2rem !important;
+        }
+    }
 }
 </style>
