2583104f2acbb183ffeef8a674e14f6ac3fb51fc
polaris <polaris.tiger803@yandex.com>
2020-11-22 20:56:43 +0200

    UI update

--- app/Console/Commands/ShareASaleInsert.php
+++ app/Console/Commands/ShareASaleInsert.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Models\Product;
+use App\Http\Repositories\ShareSale;
 use Illuminate\Console\Command;
 
 class ShareASaleInsert extends Command
@@ -38,20 +38,20 @@ class ShareASaleInsert extends Command
      */
     public function handle()
     {
-//        $shareSale = new ShareSale();
-//
-//        $shareSale->dataInsertFromAPI('shareasale.com');
-        $data = Product::where('network', 'shareasale.com')->get();
-        if ($data) {
-            foreach ($data as $row) {
-                $p_tAry = explode(' - ', $row['p_title']);
-                $pUrl = isset($p_tAry[count($p_tAry) - 1]) ? $p_tAry[count($p_tAry) - 1] : $p_tAry[0];
-                $pRow = Product::find($row->id);
-                $pRow->program_url = 'https://'. $pUrl;
-                $pRow->save();
-            }
-        }
+        $shareSale = new ShareSale();
 
-        return 0;
+        $shareSale->dataInsertFromAPI('shareasale.com');
+//        $data = Product::where('network', 'shareasale.com')->get();
+//        if ($data) {
+//            foreach ($data as $row) {
+//                $p_tAry = explode(' - ', $row['p_title']);
+//                $pUrl = isset($p_tAry[count($p_tAry) - 1]) ? $p_tAry[count($p_tAry) - 1] : $p_tAry[0];
+//                $pRow = Product::find($row->id);
+//                $pRow->program_url = 'https://'. $pUrl;
+//                $pRow->save();
+//            }
+//        }
+//
+//        return 0;
     }
 }
